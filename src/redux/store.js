ae4ce7f74ab7dcb28bbcf231c2235bf2b653d1dd
samuel shuman <81707204+kucherjavij@users.noreply.github.com>
2022-02-09 10:02:42 +0200

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,7 +4,8 @@ import contactReducer from "../contacts/contacts-reducer"
 
 
 const store = configureStore({
-    reducer: {contacts: contactReducer}
+    reducer: { contacts: contactReducer },
+    devTools: process.env.NODE_ENV !== "production",
 })
 
 export default store;
